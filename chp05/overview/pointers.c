10ac1fb981cc42a4328df927895e25283d4f607b
Derek Molloy <derek@derekmolloy.ie>
2014-12-22 13:59:18 +0000

    Update pointers.c
    Fixed typo identified on website -- Thanks Greg.

--- chp05/overview/pointers.c
+++ chp05/overview/pointers.c
@@ -4,7 +4,7 @@ int main(){
    int y = 1000;
    int *p;
    p = &y;
-   printf("The variable has rvalue %d and the lvalue %p.\n", y, &y);
+   printf("The variable has value %d and the address %p.\n", y, &y);
    printf("The pointer stores %p and points at value %d.\n", p, *p);
    printf("The pointer itself has address %p and size %d.\n", &p, sizeof(p));
    return 0;
