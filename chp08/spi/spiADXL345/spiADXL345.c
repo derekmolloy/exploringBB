2058212d9e7e65ec2545cde4efbee0f0a89a4081
Derek Molloy <molloyd@beaglebone.localdomain>
2015-04-03 15:01:51 +0100

    Fixing address issue on SPIDevice.cpp class - thanks Scott

--- chp08/spi/spiADXL345/spiADXL345.c
+++ chp08/spi/spiADXL345/spiADXL345.c
@@ -5,11 +5,11 @@
 *  To read to the ADXL345 the leading bit of the address must be
 *  high. The Device ID of the ADXL345 is 0xE5, which is 229 in decimal, so
 *  this should be the value returned. This code is based on the code at:
-*  https://www.kernel.org/doc/Documentation/spi/ 
+*  https://www.kernel.org/doc/Documentation/spi/
 *
-* Written by Derek Molloy for the book "Exploring BeagleBone: Tools and 
+* Written by Derek Molloy for the book "Exploring BeagleBone: Tools and
 * Techniques for Building with Embedded Linux" by John Wiley & Sons, 2014
-* ISBN 9781118935125. Please see the file README.md in the repository root 
+* ISBN 9781118935125. Please see the file README.md in the repository root
 * directory for copyright and GNU GPLv3 license information.            */
 
 #include<stdio.h>
@@ -25,7 +25,7 @@ static const char *device = "/dev/spidev1.0";
 
 int main(){
    unsigned char send[2], receive[2];
-   send[0]= (0x80+0x00);  //want to read address 0x00 on the ADXL345
+   send[0]= (0x80|0x00);  //want to read address 0x00 on the ADXL345
    uint8_t mode=3, bits=8;
    uint32_t speed=500000;
    int file;
--- library/bus/SPIDevice.cpp
+++ library/bus/SPIDevice.cpp
@@ -1,24 +1,24 @@
 /*
  * SPIDevice.cpp  Created on: 22 May 2014
  * Copyright (c) 2014 Derek Molloy (www.derekmolloy.ie)
- * Made available for the book "Exploring BeagleBone" 
+ * Made available for the book "Exploring BeagleBone"
  * See: www.exploringbeaglebone.com
  * Licensed under the EUPL V.1.1
  *
- * This Software is provided to You under the terms of the European 
- * Union Public License (the "EUPL") version 1.1 as published by the 
- * European Union. Any use of this Software, other than as authorized 
- * under this License is strictly prohibited (to the extent such use 
+ * This Software is provided to You under the terms of the European
+ * Union Public License (the "EUPL") version 1.1 as published by the
+ * European Union. Any use of this Software, other than as authorized
+ * under this License is strictly prohibited (to the extent such use
  * is covered by a right of the copyright holder of this Software).
- * 
- * This Software is provided under the License on an "AS IS" basis and 
- * without warranties of any kind concerning the Software, including 
- * without limitation merchantability, fitness for a particular purpose, 
- * absence of defects or errors, accuracy, and non-infringement of 
- * intellectual property rights other than copyright. This disclaimer 
- * of warranty is an essential part of the License and a condition for 
+ *
+ * This Software is provided under the License on an "AS IS" basis and
+ * without warranties of any kind concerning the Software, including
+ * without limitation merchantability, fitness for a particular purpose,
+ * absence of defects or errors, accuracy, and non-infringement of
+ * intellectual property rights other than copyright. This disclaimer
+ * of warranty is an essential part of the License and a condition for
  * the grant of any rights to this Software.
- * 
+ *
  * For more details, see http://www.derekmolloy.ie/
  */
 
@@ -104,9 +104,9 @@ unsigned char SPIDevice::readRegister(unsigned int registerAddress){
 	unsigned char send[2], receive[2];
 	memset(send, 0, sizeof send);
 	memset(receive, 0, sizeof receive);
-	send[0] = (unsigned char) (0x80 + registerAddress);
+	send[0] = (unsigned char) (0x80 | registerAddress);
 	this->transfer(send, receive, 2);
-    //cout << "The value that was received is: " << (int) receive[1] << endl;
+	//cout << "The value that was received is: " << (int) receive[1] << endl;
 	return receive[1];
 }
 
@@ -114,7 +114,7 @@ unsigned char* SPIDevice::readRegisters(unsigned int number, unsigned int fromAd
 	unsigned char* data = new unsigned char[number];
 	unsigned char send[number+1], receive[number+1];
 	memset(send, 0, sizeof send);
-	send[0] =  (unsigned char) (0x80 + 0x40 + fromAddress); //set read bit and MB bit
+	send[0] =  (unsigned char) (0x80 | 0x40 | fromAddress); //set read bit and MB bit
 	this->transfer(send, receive, number+1);
 	memcpy(data, receive+1, number);  //ignore the first (address) byte in the array returned
 	return data;
