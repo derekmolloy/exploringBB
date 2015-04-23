98d902bcd87dca81fb53a41c717e1579a192e46e
derekmolloy <molloyd@gmail.com>
2015-04-23 21:35:58 +0100

    fixing tabs for display

--- chp10/cgicc/getLED.cpp
+++ chp10/cgicc/getLED.cpp
@@ -15,37 +15,37 @@ using namespace std;
 using namespace cgicc;
 
 int main(){
-	Cgicc form;					// The Cgicc object
-	LED *led3 = new LED(3);				// The LED object -- USR3
+   Cgicc form;                                      // The Cgicc object
+   LED *led3 = new LED(3);                          // The LED object -- USR3
 
-	// Generate the response HTML page
-	char *value = getenv("REMOTE_ADDR");		// the remote address CGI env. variable
-	cout << "Content-type:text/html\r\n\r\n";	// generate the HTML output
-	cout << "<html><head>\n";
-	cout << "<title>EBB C++ GET Example</title>\n";
-	cout << "</head><body>\n";
-	cout << "<h1>BeagleBone GET Example</h1>\n";
+   // Generate the response HTML page
+   char *value = getenv("REMOTE_ADDR");	            // the remote address CGI env. variable
+   cout << "Content-type:text/html\r\n\r\n";        // generate the HTML output
+   cout << "<html><head>\n";
+   cout << "<title>EBB C++ GET Example</title>\n";
+   cout << "</head><body>\n";
+   cout << "<h1>BeagleBone GET Example</h1>\n";
 
-	form_iterator it = form.getElement("command");	// read the URL get command string
-	if (it == form.getElements().end() || it->getValue()==""){
-		cout << "<div> The LED command is missing or invalid.</div>";
-		cout << "<div> Valid commands are on, off, flash, and status </div>";
-	}
-	else{
-		string cmd(**it);
-		cout << "<div> The LED command is " << cmd << ".</div>";
-		/** This code sets the USR3 LED state using the LED class **/
-		if(cmd=="on") led3->turnOn();
-		else if(cmd=="off") led3->turnOff();
-		else if(cmd=="flash") led3->flash("100");
-		else if(cmd=="status"){
-			cout << "<div>";
-			led3->outputState();
-			cout << "</div>";
-		}
-		else cout << "<div> Invalid command! </div>";
-	}
-	cout << "<div> The CGI REMOTE_ADDR environment variable is " << value << "</div>";
-	cout << "</body></html>\n";
-	return 0;
+   form_iterator it = form.getElement("command");   // read the URL get command string
+   if (it == form.getElements().end() || it->getValue()==""){
+      cout << "<div> The LED command is missing or invalid.</div>";
+      cout << "<div> Valid commands are on, off, flash, and status </div>";
+   }
+   else{
+      string cmd(**it);
+      cout << "<div> The LED command is " << cmd << ".</div>";
+      /** This code sets the USR3 LED state using the LED class **/
+      if(cmd=="on") led3->turnOn();
+      else if(cmd=="off") led3->turnOff();
+      else if(cmd=="flash") led3->flash("100");
+      else if(cmd=="status"){
+         cout << "<div>";
+         led3->outputState();
+         cout << "</div>";
+      }
+      else cout << "<div> Invalid command! </div>";
+   }
+   cout << "<div> The CGI REMOTE_ADDR environment variable is " << value << "</div>";
+   cout << "</body></html>\n";
+   return 0;
 }
--- chp10/cgicc/postLED.cpp
+++ chp10/cgicc/postLED.cpp
@@ -17,54 +17,54 @@ using namespace std;
 using namespace cgicc;
 
 int main(){
-	Cgicc form;					// the CGI form object
-	LED *led3 = new LED(3);				// the LED object -- USR3
-	string flashPeriod, command;			// default values
+   Cgicc form;                                     // the CGI form object
+   LED *led3 = new LED(3);                         // the LED object -- USR3
+   string flashPeriod, command;                    // default values
 
-	bool isStatus = form.queryCheckbox("status");   // get the state of the status checkbox
-	form_iterator it = form.getElement("period");   // get the period text value
-	if (it == form.getElements().end() || it->getValue()==""){
-		flashPeriod = "100";			// if it is invalid use 100
-	}
-	else {	flashPeriod = it->getValue(); }		// otherwise use submitted value
-	it = form.getElement("command");		// get the radio command chosen
-	if (it == form.getElements().end() || it->getValue()==""){
-		command = "off";			// if it is invalid use "off"
-	}
-	else {	command = it->getValue(); }		// otherwise use submitted value
-	char *value = getenv("REMOTE_ADDR");		// The remote address CGI env. variable
+   bool isStatus = form.queryCheckbox("status");   // get the state of the status checkbox
+   form_iterator it = form.getElement("period");   // get the period text value
+   if (it == form.getElements().end() || it->getValue()==""){
+      flashPeriod = "100";                         // if it is invalid use 100
+   }
+   else { flashPeriod = it->getValue(); }          // otherwise use submitted value
+   it = form.getElement("command");                // get the radio command chosen
+   if (it == form.getElements().end() || it->getValue()==""){
+      command = "off";                             // if it is invalid use "off"
+   }
+   else { command = it->getValue(); }              // otherwise use submitted value
+   char *value = getenv("REMOTE_ADDR");	           // The remote address CGI env. variable
 
-	// Generate the form but use states that are set in the form that was submitted
-	cout << HTTPHTMLHeader() << endl;		// Generate the HTML form using cgicc
-	cout << html() << head() << title("EBB C++ Post LED Example") << head() << endl;
-	cout << body() << h1("BeagleBone POST LED Example") << endl;;
-	cout << "<form action=\"/cgi-bin/postLED.cgi\" method=\"POST\">\n";
-	cout << "<div>LED state: <input type=\"radio\" name=\"command\" value=\"on\""
-	     << ( command=="on" ? "checked":"") << "/> On ";	// is the command="on"?
-	cout << "<input type=\"radio\" name=\"command\" value=\"off\""
-	     << ( command=="off" ? "checked":"") << "/> Off ";
-	cout << "<input type=\"radio\" name=\"command\" value=\"flash\""
-	     << ( command=="flash" ? "checked":"") << "/> Flash ";
-	cout << "<input type=\"checkbox\" name=\"status\" " << (isStatus ? "checked":"")
- 	     << " /> Display Status </div>";
-	cout << "<div>Flash period: <input type=\"text\" name=\"period\" size=\"6\" value=\""
-	     << flashPeriod << "\"> ms   ";   // populate the text field
-	cout << "<input type=\"submit\" value=\"Execute Command\" />";
-	cout << "</div></form>";
+   // Generate the form but use states that are set in the form that was submitted
+   cout << HTTPHTMLHeader() << endl;               // Generate the HTML form using cgicc
+   cout << html() << head() << title("EBB C++ Post LED Example") << head() << endl;
+   cout << body() << h1("BeagleBone POST LED Example") << endl;;
+   cout << "<form action=\"/cgi-bin/postLED.cgi\" method=\"POST\">\n";
+   cout << "<div>LED state: <input type=\"radio\" name=\"command\" value=\"on\""
+        << ( command=="on" ? "checked":"") << "/> On ";	// is the command="on"?
+   cout << "<input type=\"radio\" name=\"command\" value=\"off\""
+        << ( command=="off" ? "checked":"") << "/> Off ";
+   cout << "<input type=\"radio\" name=\"command\" value=\"flash\""
+        << ( command=="flash" ? "checked":"") << "/> Flash ";
+   cout << "<input type=\"checkbox\" name=\"status\" " << (isStatus ? "checked":"")
+        << " /> Display Status </div>";
+   cout << "<div>Flash period: <input type=\"text\" name=\"period\" size=\"6\" value=\""
+        << flashPeriod << "\"> ms   ";   // populate the text field
+   cout << "<input type=\"submit\" value=\"Execute Command\" />";
+   cout << "</div></form>";
 
-	// Process the form data to trigger the LED state
-	if (command=="on") led3->turnOn();			// turn the LED on?
-	else if (command=="off") led3->turnOff();		// off?
-	else if (command=="flash") led3->flash(flashPeriod);	// flash with the period above
-	else cout << "<div> Invalid command! </div>";		// not possible at the moment
-	// If the Display Status checkbox is checked then display the status now
-	// this should happen after the command is set, otherwise it is old data
-	if (isStatus){
-		cout << "<div>";
-		led3->outputState();
-		cout << "</div>";
-	}
-	cout << "<div> The CGI REMOTE_ADDR environment variable is " << value << "</div>";
-	cout << body() << html();
-	return 0;
+   // Process the form data to trigger the LED state
+   if (command=="on") led3->turnOn();                   // turn the LED on?
+   else if (command=="off") led3->turnOff();            // off?
+   else if (command=="flash") led3->flash(flashPeriod);	// flash with the period above
+   else cout << "<div> Invalid command! </div>";        // not possible at the moment
+   // If the Display Status checkbox is checked then display the status now
+   // this should happen after the command is set, otherwise it is old data
+   if (isStatus){
+      cout << "<div>";
+      led3->outputState();
+      cout << "</div>";
+   }
+   cout << "<div> The CGI REMOTE_ADDR environment variable is " << value << "</div>";
+   cout << body() << html();
+   return 0;
 }
