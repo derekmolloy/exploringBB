b8213cc83059d350502f75acf7f4ce9408414d65
Derek Molloy <molloyd@gmail.com>
2018-07-14 00:48:02 +0100

    added adafruit code example

---
+++ chp11/adafruit/publish.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string.h>
+#include "MQTTClient.h"
+using namespace std;
+
+//Please replace the username, feed and token with your values
+#define ADDRESS    "tcp://io.adafruit.com"
+#define CLIENTID   "Beagle1"
+#define TOPIC      "molloyd/feeds/weather.temperature"
+#define AUTHMETHOD "molloyd"
+#define AUTHTOKEN  "feb8ea6179c64b638b35b93ef87d4b42"
+#define QOS        1
+#define TIMEOUT    10000L
+#define ADC_PATH   "/sys/bus/iio/devices/iio:device0/in_voltage"
+#define ADC        0
+
+float getTemperature(int adc_value) {     // from the TMP36 datasheet
+   float cur_voltage = adc_value * (1.80f/4096.0f); // Vcc = 1.8V, 12-bit
+   float diff_degreesC = (cur_voltage-0.75f)/0.01f;
+   return (25.0f + diff_degreesC);
+}
+
+int readAnalog(int number){
+   stringstream ss;
+   ss << ADC_PATH << number << "_raw";
+   fstream fs;
+   fs.open(ss.str().c_str(), fstream::in);
+   fs >> number;
+   fs.close();
+   return number;
+}
+
+int main(int argc, char* argv[]) {
+   char str_payload[100];          // Set your max message size here
+   MQTTClient client;
+   cout << "Starting Beagle board MQTT Adafruit Publish Example" << endl;
+   MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+   MQTTClient_message pubmsg = MQTTClient_message_initializer;
+   MQTTClient_deliveryToken token;
+   MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+   opts.keepAliveInterval = 20;
+   opts.cleansession = 1;
+   opts.username = AUTHMETHOD;
+   opts.password = AUTHTOKEN;
+   int rc;
+   if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
+      cout << "Failed to connect, return code " << rc << endl;
+      return -1;
+   }
+   sprintf(str_payload, "%f", getTemperature(readAnalog(ADC)));
+   pubmsg.payload = str_payload;
+   pubmsg.payloadlen = strlen(str_payload);
+   pubmsg.qos = QOS;
+   pubmsg.retained = 0;
+   MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+   cout << "Waiting for up to " << (int)(TIMEOUT/1000) <<
+        " seconds for publication of " << str_payload <<
+        " \non topic " << TOPIC << " for ClientID: " << CLIENTID << endl;
+   rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+   if(rc == MQTTCLIENT_SUCCESS) {
+     cout << "Message with token " << (int)token << " delivered." << endl;
+   }
+   else {
+     cout << "Did not complete with error code: " << rc << endl;
+     // MQTTCLIENT_SUCCESS 0           MQTTCLIENT_FAILURE -1
+     // MQTTCLIENT_DISCONNECTED -3     MQTTCLIENT_MAX_MESSAGES_INFLIGHT -4
+     // MQTTCLIENT_BAD_UTF8_STRING -5  MQTTCLIENT_NULL_PARAMETER -6
+     // MQTTCLIENT_TOPICNAME_TRUNCATED -7   MQTTCLIENT_BAD_STRUCTURE -8
+     // MQTTCLIENT_BAD_QOS   -9        MQTTCLIENT_SSL_NOT_SUPPORTED   -10
+   }
+   MQTTClient_disconnect(client, 10000);
+   MQTTClient_destroy(&client);
+   return rc;
+}
+
---
+++ chp11/adafruit/subscribe.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "MQTTClient.h"
+using namespace std;
+
+#define ADDRESS     "tcp://io.adafruit.com:1883"
+#define CLIENTID    "Beagle2"
+#define TOPIC       "molloyd/feeds/weather.temperature"
+#define AUTHMETHOD  "molloyd"
+#define AUTHTOKEN   "feb8ea6179c64b638b35b93ef87d4b42"
+#define QOS         1
+#define TIMEOUT     10000L
+#define LED_GPIO    "/sys/class/gpio/gpio60/"
+#define THRESHOLD   30
+
+// Use this function to control the LED
+void writeGPIO(string filename, string value){
+   fstream fs;
+   string path(LED_GPIO);
+   fs.open((path + filename).c_str(), fstream::out);
+   fs << value;
+   fs.close();
+}
+
+volatile MQTTClient_deliveryToken deliveredtoken;
+
+void delivered(void *context, MQTTClient_deliveryToken dt) {
+    printf("Message with token value %d delivery confirmed\n", dt);
+    deliveredtoken = dt;
+}
+
+int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
+    int i;
+    char* payloadptr;
+    printf("Message arrived\n");
+    printf("     topic: %s\n", topicName);
+    printf("   message: ");
+    payloadptr = (char*) message->payload;
+    float temperature = atof(payloadptr);
+    printf("The temperature is %f\n", temperature);
+    temperature > THRESHOLD ? writeGPIO("value", "1") : writeGPIO("value", "0");
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+    return 1;
+}
+
+void connlost(void *context, char *cause) {
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
+}
+
+int main(int argc, char* argv[]) {
+    MQTTClient client;
+    MQTTClient_connectOptions opts = MQTTClient_connectOptions_initializer;
+    int rc;
+    int ch;
+    writeGPIO("direction", "out");
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    opts.keepAliveInterval = 20;
+    opts.cleansession = 1;
+    opts.username = AUTHMETHOD;
+    opts.password = AUTHTOKEN;
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+    if ((rc = MQTTClient_connect(client, &opts)) != MQTTCLIENT_SUCCESS) {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
+    MQTTClient_subscribe(client, TOPIC, QOS);
+
+    do {
+        ch = getchar();
+    } while(ch!='Q' && ch != 'q');
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+}
+
