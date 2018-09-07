253a70aefec3a95d2c4fa3974fdcb6218dfd4ab5
Derek Molloy <molloyd@gmail.com>
2018-09-07 05:27:06 +0100

    updating pru code

--- chp15/pru/blinkLED/gen/PRU_gpioToggle.pp
+++
@@ -1,7 +0,0 @@
-gen/PRU_gpioToggle.object: PRU_gpioToggle.c
-gen/PRU_gpioToggle.object: /usr/share/ti/cgt-pru/include/stdint.h
-gen/PRU_gpioToggle.object: /usr/lib/ti/pru-software-support-package/include/am335x/pru_cfg.h
-gen/PRU_gpioToggle.object: resource_table_empty.h
-gen/PRU_gpioToggle.object: /usr/share/ti/cgt-pru/include/stddef.h
-gen/PRU_gpioToggle.object: /usr/lib/ti/pru-software-support-package/include/rsc_types.h
-gen/PRU_gpioToggle.object: /usr/lib/ti/pru-software-support-package/include/pru_types.h
--- chp15/pru/blinkLED/gen/blinkLED.pp
+++
@@ -1,7 +0,0 @@
-gen/blinkLED.object: blinkLED.c
-gen/blinkLED.object: /usr/share/ti/cgt-pru/include/stdint.h
-gen/blinkLED.object: /usr/lib/ti/pru-software-support-package/include/am335x/pru_cfg.h
-gen/blinkLED.object: resource_table_empty.h
-gen/blinkLED.object: /usr/share/ti/cgt-pru/include/stddef.h
-gen/blinkLED.object: /usr/lib/ti/pru-software-support-package/include/rsc_types.h
-gen/blinkLED.object: /usr/lib/ti/pru-software-support-package/include/pru_types.h
