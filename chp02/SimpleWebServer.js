fcac38b647cfba9c0a464dff3314a3727d0fdd3b
Derek Molloy <molloyd@gmail.com>
2018-10-21 21:07:14 +0100

    Update SimpleWebServer.js

--- chp02/SimpleWebServer.js
+++ chp02/SimpleWebServer.js
@@ -1,8 +1,6 @@
 // A Simple Example Node.js Webserver Running on Port 5050
 // Written by Derek Molloy for the book "Exploring BeagleBone: Tools and 
-// Techniques for Building with Embedded Linux" by John Wiley & Sons, 2014
-// ISBN 9781118935125. Please see the file README.md in the repository root 
-// directory for copyright and GNU GPLv3 license information.   
+// Techniques for Building with Embedded Linux" 
 
 var http = require('http');  // require the http module
 var server = http.createServer(
