ddf0fcea834791e306c0570f0eca1bdf3718dd6c
Derek Molloy <molloyd@gmail.com>
2018-10-21 21:06:57 +0100

    Update BlinkLED3.js

--- chp02/BlinkLED3.js
+++ chp02/BlinkLED3.js
@@ -1,8 +1,6 @@
 // Example application for flashing an LED using BoneScript
 // Written by Derek Molloy for the book "Exploring BeagleBone: Tools and 
-// Techniques for Building with Embedded Linux" by John Wiley & Sons, 2014
-// ISBN 9781118935125. Please see the file README.md in the repository root 
-// directory for copyright and GNU GPLv3 license information.   
+// Techniques for Building with Embedded Linux"
 
 var b = require('bonescript'); //using BoneScript
 var LED3Pin = "USR3";          //USR3 is D5 on the BBB
